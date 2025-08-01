6379f744a177c93a4524891097e42ecd9341d334
yacine arkane eddine <khammemzine@gmail.com>
2025-08-02 00:03:48 +0100

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(session({
+  secret: "my-secret",
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "1234") {
+    req.session.authenticated = true;
+    res.redirect("/members");
+  } else {
+    res.send("اسم المستخدم أو كلمة المرور خاطئة");
+  }
+});
+
+app.get("/members", (req, res) => {
+  if (req.session.authenticated) {
+    res.sendFile(path.join(__dirname, "views", "members.html"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/download", (req, res) => {
+  if (req.session.authenticated) {
+    res.sendFile(path.join(__dirname, "public", "secret.pdf"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
